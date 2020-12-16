0e1c051bbc723fc65240ef668615fcf9117fee15
Zhenning Ruan <rzning@outlook.com>
2020-12-16 21:53:31 +0800

    🛸 参考 create-react-app 模板初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # try-react-terser-repl
 Terser REPL Demo
+
+- 参考 [Create React App][cra] 官方基础模板 [cra-template] 初始化项目。
+
+
+[cra]: <https://github.com/facebook/create-react-app>
+[cra-template]: <https://github.com/facebook/create-react-app/tree/master/packages/cra-template>
---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import logo from '../assets/terser-logo.png'
+import styles from './App.module.css'
+
+export default function App () {
+  return (
+    <div className="app">
+      <header className={styles.header}>
+        <img src={logo} alt="terser-logo"/>
+        <span>Terser REPL Demo</span>
+      </header>
+    </div>
+  )
+}
