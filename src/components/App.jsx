3a533ee86a278ab44c6f3da7b045fdf824f3c4d6
Zhenning Ruan <rzning@outlook.com>
2021-01-08 01:27:03 +0800

    🌈 使用 Prettier 规范代码格式

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import logo from '../assets/terser-logo.png'
 import styles from './App.module.css'
 
-export default function App () {
+export default function App() {
   return (
     <div className="app">
       <header className={styles.header}>
-        <img src={logo} alt="terser-logo"/>
+        <img src={logo} alt="terser-logo" />
         <span>Terser REPL Demo</span>
       </header>
     </div>
